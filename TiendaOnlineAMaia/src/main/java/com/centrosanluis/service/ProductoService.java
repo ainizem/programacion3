2fff5e9a3c749bf65f7c79e2badf45107a5a6562
ainizem <ainizemesquitac@gmail.com>
2025-03-18 16:26:55 +0100

    Add files via upload

---
+++ TiendaOnlineAMaia/src/main/java/com/centrosanluis/service/ProductoService.java
@@ -0,0 +1,18 @@
+package com.centrosanluis.service;
+
+import java.util.List;
+
+import com.centrosanluis.dao.ProductoDAO;
+import com.centrosanluis.model.Producto;
+
+public class ProductoService {
+	
+	ProductoDAO productoBD = new ProductoDAO();
+	
+	
+	public List<Producto> getProductos() {
+		return productoBD.getProductos();
+	}
+	
+
+}
